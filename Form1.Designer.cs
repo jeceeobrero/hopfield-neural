68a311cee71477d4afe12a63fae600f7c57ae7f9
jeceeobrero <79010954+jeceeobrero@users.noreply.github.com>
2022-04-30 19:51:51 +0800

    updated alphabet hopfield

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace HNN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.addPatternBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 333);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 27);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox5);
+            this.groupBox1.Controls.Add(this.pictureBox6);
+            this.groupBox1.Controls.Add(this.pictureBox7);
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Location = new System.Drawing.Point(27, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 250);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Patterns";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(30, 137);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(92, 85);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(146, 137);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(92, 85);
+            this.pictureBox6.TabIndex = 4;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(262, 137);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(92, 85);
+            this.pictureBox7.TabIndex = 3;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(262, 32);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(92, 85);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(146, 32);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(92, 85);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(30, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(92, 85);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.clearBtn);
+            this.groupBox3.Controls.Add(this.addPatternBtn);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.pictureBox1);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Location = new System.Drawing.Point(438, 26);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(233, 372);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Result";
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(119, 305);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(90, 23);
+            this.clearBtn.TabIndex = 10;
+            this.clearBtn.Text = "Clear Pattern";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // addPatternBtn
+            // 
+            this.addPatternBtn.Location = new System.Drawing.Point(23, 305);
+            this.addPatternBtn.Name = "addPatternBtn";
+            this.addPatternBtn.Size = new System.Drawing.Size(90, 23);
+            this.addPatternBtn.TabIndex = 9;
+            this.addPatternBtn.Text = "Add Pattern";
+            this.addPatternBtn.UseVisualStyleBackColor = true;
+            this.addPatternBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Number of Patterns: 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Size of Neural Network: 100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Distort Level: 0 %";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 190);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(30, 38);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(324, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.trackBar1);
+            this.groupBox2.Location = new System.Drawing.Point(27, 298);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(379, 100);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Distort Level";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(687, 433);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TEAM JIMZ RED: Alvarez, Llanos, Obrero - HNN Activity";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addPatternBtn;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using HopfieldNeuralNetwork;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HNN
+{
+    public partial class Form1 : Form
+    {
+        private int noOfNeurons = 100;
+        private int m_iSize = 20;
+        Bitmap image = null;
+
+        private NeuralNetwork NN;
+        private List<Neuron> pattern;
+        private List<Neuron> initialState;
+        private List<Neuron> graphic;
+
+        private int numPat = 0;
+        private int selected = 0;
+
+        private List<String> fileName;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pattern = new List<Neuron>(noOfNeurons);
+            initialState = new List<Neuron>(noOfNeurons);
+            NN = new NeuralNetwork(noOfNeurons);
+            fileName = new List<String>();
+            setInitialStatePattern();
+            graphic = initialState;
+        }
+
+        private void setInitialStatePattern()
+        {
+            //...some initialState forming code goes
+            Random rnd = new Random();
+            for (int i = 0; i < noOfNeurons; i++)
+            {
+                Neuron neuron = new Neuron();
+                initialState.Add(neuron);
+
+                int bit;
+                bit = rnd.Next(2);
+                if (bit == 0) initialState[i].State = NeuronStates.AlongField;
+                else
+                if (bit == 1) initialState[i].State = NeuronStates.AgainstField;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics myGraphics = e.Graphics;
+            if(graphic != null)
+            {
+                for (int i = 0; i < graphic.Count; i++)
+                {
+                   if(graphic[i].State == -1)
+                    {
+                        myGraphics.FillRectangle(new SolidBrush(Color.Black), (i/10) * m_iSize, (i%10) * m_iSize, m_iSize, m_iSize);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numPat == 6) MessageBox.Show("Only up to 6 patterns are to be stored in Jimz Red's Neural Network.");
+            else openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            image = new Bitmap(openFileDialog1.FileName);
+            bool flag = true;
+            for (int i = 0; i < fileName.Count; i++)
+            {
+                if (fileName[i] == openFileDialog1.FileName)
+                {
+                    DialogResult dr = MessageBox.Show("The pattern you chose is already in the Jimz Red's Neural Network. Confirming this will only give more bias to this pattern. Do you want to continue or not?", "Warning!", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (dr == DialogResult.No) flag = false;
+                    break;
+                }
+            }
+
+            if(flag == true)
+            {
+                numPat++;
+                label1.Text = "Number of Patterns: " + numPat;
+                fileName.Add(openFileDialog1.FileName);
+
+                //...some pattern forming codes
+                correctPattern();
+                NN.AddPattern(pattern);
+
+                //display loaded image in picture box
+                displayImage(numPat);
+                this.Refresh();
+            }
+        }
+
+        private void correctPattern()
+        {
+            for (int i = 0; i < noOfNeurons; i++)
+            {
+                Neuron neuron = new Neuron();
+                pattern.Add(neuron);
+                int bit = 0;
+                if (image.GetPixel(i / image.Width, i % image.Width).GetBrightness() < 0.02)
+                    bit = 1;
+
+                if (bit == 0) pattern[i].State = NeuronStates.AlongField;
+                else
+                    if (bit == 1) pattern[i].State = NeuronStates.AgainstField;
+            }
+        }
+
+        private void distortPattern()
+        {
+            int num = trackBar1.Value * 2;
+            Random r = new Random();
+            int p = 0;
+
+            for (int i=0; i<num; i++)
+            {
+                p = r.Next(noOfNeurons);
+                if(initialState[p].State == 1)
+                    initialState[p].State = NeuronStates.AgainstField;
+                else
+                    initialState[p].State = NeuronStates.AlongField;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(numPat == 0)
+            {
+                MessageBox.Show("Add patterns first to the Jimz Red's Neural Network.");
+            }
+            else if(selected == 0){
+                MessageBox.Show("Select a pattern first to run.");
+            }
+            else
+            {
+                NN.Run(initialState);
+                timer1.Enabled = true;
+                trackBar1.Value = 0;
+                label2.Text = "Distort Level: " + trackBar1.Value * 10 + "%";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphic = NN.Neurons;
+            this.Refresh();
+            timer1.Enabled = false;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if(numPat == 0) MessageBox.Show("Add patterns first to the Jimz Red's Neural Network.");
+            else if(selected == 0) MessageBox.Show("Select a pattern to distort.");
+            if (trackBar1.Value!=0) distortPattern();
+            label2.Text = "Distort Level: " + trackBar1.Value * 10 + "%";
+            graphic = initialState;
+            this.Refresh();
+        }
+
+        private void displayImage(int num)
+        {
+            if (image != null)
+            {
+                Bitmap bmp = image;
+                Bitmap zoomed;
+                int width = 1, height = 1;
+
+                switch (num)
+                {
+                    case 1: width = pictureBox2.Width; height = pictureBox2.Height; break;
+                    case 2: width = pictureBox3.Width; height = pictureBox3.Height; break;
+                    case 3: width = pictureBox4.Width; height = pictureBox4.Height; break;
+                    case 4: width = pictureBox5.Width; height = pictureBox5.Height; break;
+                    case 5: width = pictureBox6.Width; height = pictureBox6.Height; break;
+                    case 6: width = pictureBox7.Width; height = pictureBox7.Height; break;
+                }
+
+                zoomed = new Bitmap((int)(width), (int)(height));
+
+                using (Graphics g = Graphics.FromImage(zoomed))
+                {
+                    g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    g.DrawImage(bmp, new Rectangle(Point.Empty, zoomed.Size));
+                }
+
+                switch (num)
+                {
+                    case 1: pictureBox2.Image = zoomed;break;
+                    case 2: pictureBox3.Image = zoomed; break;
+                    case 3: pictureBox4.Image = zoomed; break;
+                    case 4: pictureBox5.Image = zoomed; break;
+                    case 5: pictureBox6.Image = zoomed; break;
+                    case 6: pictureBox7.Image = zoomed; break;
+                }
+            }
+        }
+               
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            selected = 1;
+            image = new Bitmap(fileName[0]);
+            pictureboxHelper();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            selected = 1;
+            image = new Bitmap(fileName[1]);
+            pictureboxHelper();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            selected = 1;
+            image = new Bitmap(fileName[2]);
+            pictureboxHelper();
+        }
+
+        
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            selected = 1;
+            image = new Bitmap(fileName[3]);
+            pictureboxHelper();
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            selected = 1;
+            image = new Bitmap(fileName[4]);
+            pictureboxHelper();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            selected = 1;
+            image = new Bitmap(fileName[5]);
+            pictureboxHelper();
+        }
+
+        private void pictureboxHelper()
+        {
+            correctPattern();
+            initialState = pattern;
+            graphic = initialState;
+            this.Refresh();
+            trackBar1.Value = 0;
+        }
+
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            selected = 1;
+            NN.FreeMatrix();
+            pattern.Clear();
+            fileName.Clear();
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox4.Image = null;
+            pictureBox5.Image = null;
+            pictureBox6.Image = null;
+            pictureBox7.Image = null;
+            trackBar1.Value = 0;
+            label2.Text = "Distort Level: 0 %";
+            numPat = 0;
+            label1.Text = "Number of Patterns: 0";
+            setInitialStatePattern();
+            graphic = initialState;
+            this.Refresh();
+        }
+    }
+}
